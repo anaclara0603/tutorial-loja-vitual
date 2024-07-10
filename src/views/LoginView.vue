e82f6c2e579011b7d99d2d89190f11f5f2848a55
anaclara06 <anaclara.hahn1@gmail.com>
2024-07-10 15:11:15 -0300

    Add Passage Identity authentication and elements dependencies

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,12 @@
-<script setup></script>
+<script setup>
+import '@passageidentity/passage-elements/passage-auth';
+</script>
+
 <template>
   <h1>Login</h1>
+  <div class="authContainer">
+    <passage-auth app-id="Q1su0aOisZlfvz3p9xhcPhjf"></passage-auth>
+  </div>
 </template>
+
+<style></style>
